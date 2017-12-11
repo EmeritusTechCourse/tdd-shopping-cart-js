654dfdfd54bdbfef10fb27e7b6782c54b358d13f
RachelKoldenhoven <rachel@koldenfrozen.com>
2017-12-11 10:20:13 +0100

    initial cart is empty

--- test/Cart.test.js
+++ test/Cart.test.js
@@ -1,10 +1,26 @@
-const Cart = require('../src/Cart.js')
+const Cart = require('../src/Cart.js');
+const expect = require('chai').expect;
 
 describe('Cart', () => {
     it('should initialize as empty', () => {
         const cart = new Cart()
 
-        expect(cart.items).toEqual([])
-        expect(cart.totalPrice).toEqual(0)
+        expect(cart.items).to.deep.equal([])
+        expect(cart.totalPrice).to.be.equal(0)
     })
+
+
+    // Given I have an empty cart, when I add an Item, then I expect
+    // to see totalPrice reflect the sum of all the Items in my cart,
+    // times the quantities of each item.
+
+    it('should know the total price of items in my cart', () => {
+        // Setup
+        const cart = new Cart();
+        
+        // Exercise
+
+        // Assert
+    })
+
 })
