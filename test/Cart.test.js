3e4b02866bb6cbc2bb852f724cc787176c59d511
Brent Gardner <bgardner@squarelabs.net>
2018-06-06 09:48:37 +0530

    Failing addItem test

--- test/Cart.test.js
+++ test/Cart.test.js
@@ -1,11 +1,27 @@
 const Cart = require('../src/Cart.js');
+const Item = require('../src/Item.js');
 const expect = require('chai').expect;
 
 describe('Cart', () => {
     it('should initialize as empty', () => {
+        // Setup & exercise
         const cart = new Cart()
 
+        // Assert
         expect(cart.items).to.deep.equal([])
         expect(cart.totalPrice).to.be.equal(0)
+    })
+
+    it('should reflect total price', () => {
+        // setup
+        const cart = new Cart()
+        const expected = 20000;
+
+        // exercise
+        const item = new Item('Watch', 10000, false);
+        cart.addItem(item, 2);
+
+        // assert
+        expect(cart.totalPrice).to.equal(expected);
     })
 })
